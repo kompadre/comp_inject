4b8b722fcaa0228e135615d5a9f64bc88c8c2ac1
Alexey Kompaniets <alexey@anunzen.com>
2021-08-10 14:36:07 +0200

    memory improvements

--- comp_inject.c
+++ comp_inject.c
@@ -15,13 +15,22 @@
 	ZEND_PARSE_PARAMETERS_END()
 #endif
 
-static zend_op_array *ci_compile_file(zend_file_handle *file_handle, int type);
+zend_op_array *ci_compile_file(zend_file_handle *file_handle, int type);
 zend_op_array *(*original_compile_file)(zend_file_handle *file_handle, int type);
 
+
 ZEND_BEGIN_MODULE_GLOBALS(comp_inject)
-    zval inject_start;
-    zval inject_end;
+    unsigned char started;
+    unsigned char compiling;
+    //zend_string *inject_start_string;
+    //zend_string *inject_end_string;
 ZEND_END_MODULE_GLOBALS(comp_inject)
+
+
+unsigned char started;
+unsigned char compiling;
+zend_string *inject_start_string;
+zend_string *inject_end_string;
 
 ZEND_DECLARE_MODULE_GLOBALS(comp_inject)
 
@@ -32,82 +41,151 @@ ZEND_DECLARE_MODULE_GLOBALS(comp_inject)
 #define COMPI_G(v) (comp_inject_globals.v)
 #endif
 
-static zend_op_array *ci_compile_file(zend_file_handle *file_handle, int type) /* {{{ */
+zend_op_array *ci_compile_file(zend_file_handle *file_handle, int type) /* {{{ */
 {
-    zend_op_array *op_array;
-    zval pv;
-    zval retval;                                 /* return value */
-
-    
-    if (!file_handle) {
-        return original_compile_file(file_handle, type);
+    if(!started || (type != ZEND_INCLUDE && type != ZEND_REQUIRE)) {
+        return compile_file(file_handle, type);
     }
 
-    zend_first_try {
+    int filenamelen = strlen(file_handle->filename);
+    if (NULL != inject_start_string && filenamelen > 0) {
+        php_printf(ZSTR_VAL(inject_start_string), file_handle->filename);
+    }
+    if (NULL == inject_end_string || !filenamelen) {
+        return compile_file(file_handle, type);
+    }
 
-    if (Z_TYPE_INFO(COMPI_G(inject_start)) == IS_STRING) {
-            op_array = zend_compile_string( &COMPI_G(inject_start), (char *)file_handle->filename);
-            if (op_array) {
-                zend_execute(op_array, &retval);
-                destroy_op_array(op_array);
-                efree_size(op_array, sizeof(zend_op_array));
+
+
+
+    /*
+
+        EG(no_extensions)=1;
+
+        new_op_array->scope = zend_get_executed_scope();
+
+        zend_try {
+            ZVAL_UNDEF(&local_retval);
+            zend_execute(new_op_array, &local_retval);
+        } zend_catch {
+            destroy_op_array(new_op_array);
+            efree_size(new_op_array, sizeof(zend_op_array));
+            zend_bailout();
+        } zend_end_try();
+
+        if (Z_TYPE(local_retval) != IS_UNDEF) {
+            if (retval_ptr) {
+                ZVAL_COPY_VALUE(retval_ptr, &local_retval);
+            } else {
+                zval_ptr_dtor(&local_retval);
             }
-    }
+        } else {
+            if (retval_ptr) {
+                ZVAL_NULL(retval_ptr);
+            }
+        }
 
-    op_array = original_compile_file(file_handle, type);
+        EG(no_extensions)=0;
+        zend_destroy_static_vars(new_op_array);
+        destroy_op_array(new_op_array);
+        efree_size(new_op_array, sizeof(zend_op_array));
+        retval = SUCCESS;
 
-    if (Z_TYPE_INFO(COMPI_G(inject_end)) == IS_STRING) {
-        if (op_array) {
-            zend_execute(op_array, &retval);
+
+    */
+
+
+
+    zend_op_array *op_array;
+    zval result;
+    op_array = compile_file(file_handle, type);
+    if (op_array) {
+        EG(no_extensions)=1;
+        op_array->scope = zend_get_executed_scope();
+
+        zend_try {
+            ZVAL_UNDEF(&result);
+            zend_execute(op_array, &result);
+        } zend_catch {
             destroy_op_array(op_array);
             efree_size(op_array, sizeof(zend_op_array));
+            zend_bailout();
+        } zend_end_try();
+
+        if (Z_TYPE(result) != IS_UNDEF) {
+            zval_ptr_dtor(&result);
         }
-       op_array = zend_compile_string( &COMPI_G(inject_end), (char *)file_handle->filename);
+
+        EG(no_extensions)=0;
+
+        if (ZEND_MAP_PTR(op_array->static_variables_ptr)) {
+            HashTable *ht = ZEND_MAP_PTR_GET(op_array->static_variables_ptr);
+            if (ht) {
+                zend_array_destroy(ht);
+                ZEND_MAP_PTR_SET(op_array->static_variables_ptr, NULL);
+            }
+        }
+
+        destroy_op_array(op_array);
+        efree_size(op_array, sizeof(zend_op_array));
+
+        // zend_destroy_file_handle(file_handle);
     }
 
-    } zend_end_try();
+    php_printf(ZSTR_VAL(inject_end_string), file_handle->filename);
+
+    const char *emptrystring = " ";
+    ZVAL_STRING(&result, " ");
+    op_array = zend_compile_string(&result, "__NOWHERE__");
 
     return op_array;
 }
 
 PHP_FUNCTION(comp_inject_stop)
 {
-    zend_compile_file = original_compile_file;
+    if (started)
+    {
+        if (inject_start_string != NULL) {
+            zend_string_release(inject_start_string);
+        }
+        if (inject_end_string != NULL) {
+            zend_string_release(inject_end_string);
+        }
+    }
+    started = 0;
 }
 
 /* {{{ string comp_inject_start( [ string $start, string $end ] )
  */
 PHP_FUNCTION(comp_inject_start)
 {
-	char *start = "";
+    if (started == 1) { RETURN_EMPTY_STRING(); }
+    started = 1;
+
+	char *start;
 	size_t start_len = 0;
-	char *end = "";
+	char *end;
 	size_t end_len = 0;
+	started = 1;
 
 	zend_string *retval;
 
-	ZEND_PARSE_PARAMETERS_START(0, 2)
-		Z_PARAM_OPTIONAL
+	ZEND_PARSE_PARAMETERS_START(1, 2)
 		Z_PARAM_STRING(start, start_len)
 		Z_PARAM_OPTIONAL
 		Z_PARAM_STRING(end, end_len)
 	ZEND_PARSE_PARAMETERS_END();
 
+
 	if (start_len > 0) {
-	    // Z_STR(COMPI_G(inject_start)) = zend_string_alloc(start_len, 0);
-	    memcpy(Z_STRVAL(COMPI_G(inject_start)), start, start_len);
-        // Z_STRVAL(COMPI_G(inject_start))[start_len] = '\0';
-	    Z_TYPE_INFO(COMPI_G(inject_start)) = IS_STRING;
-	}
-	if (end_len > 0) {
-	    // Z_STR(COMPI_G(inject_end)) = zend_string_alloc(end_len, 0);
-	    memcpy(Z_STRVAL(COMPI_G(inject_end)), end, end_len);
-        Z_STRVAL(COMPI_G(inject_end))[start_len] = '\0';
-	    Z_TYPE_INFO(COMPI_G(inject_end)) = IS_STRING;
+	    inject_start_string = zend_string_init(start, start_len, 1);
+        zend_string_hash_val(inject_start_string);
 	}
 
-    original_compile_file = zend_compile_file;
-    zend_compile_file = ci_compile_file;
+	if (end_len > 0) {
+	    inject_end_string = zend_string_init(end, end_len, 1);
+        zend_string_hash_val(inject_end_string);
+    }
 }
 /* }}}*/
 
@@ -121,13 +199,28 @@ PHP_FUNCTION(comp_inject_start)
  */
 PHP_RINIT_FUNCTION(comp_inject)
 {
+
 #if defined(ZTS) && defined(COMPILE_DL_COMP_INJECT)
 	ZEND_TSRMLS_CACHE_UPDATE();
 #endif
-
+    started = 0;
 	return SUCCESS;
 }
 /* }}} */
+
+PHP_RSHUTDOWN_FUNCTION(comp_inject)
+{
+    if (started)
+    {
+        if (inject_start_string != NULL) {
+            zend_string_release(inject_start_string);
+        }
+        if (inject_end_string != NULL) {
+            zend_string_release(inject_end_string);
+        }
+    }
+    started = 0;
+}
 
 /* {{{ PHP_MINFO_FUNCTION
  */
@@ -141,8 +234,11 @@ PHP_MINFO_FUNCTION(comp_inject)
 
 /* {{{ arginfo
  */
+
+
 ZEND_BEGIN_ARG_INFO(arginfo_comp_inject_start, 0)
-	ZEND_ARG_INFO(0, str)
+	ZEND_ARG_INFO(0, start)
+	ZEND_ARG_INFO(0, end)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO(arginfo_comp_inject_stop, 0)
@@ -161,10 +257,35 @@ static const zend_function_entry comp_inject_functions[] = {
 
 PHP_GINIT_FUNCTION(comp_inject)
 {
+    comp_inject_globals->started=0;
+    /*
     Z_STR(comp_inject_globals->inject_start) = zend_string_alloc(1024, 0);
     memset(Z_STR(comp_inject_globals->inject_start), 0, 1024);
     Z_STR(comp_inject_globals->inject_end)   = zend_string_alloc(1024, 0);
     memset(Z_STR(comp_inject_globals->inject_end), 0, 1024);
+    */
+}
+
+PHP_MINIT_FUNCTION(comp_inject){
+    started = 0;
+    inject_start_string = NULL;
+    inject_end_string   = NULL;
+
+    original_compile_file = zend_compile_file;
+    zend_compile_file = ci_compile_file;
+}
+
+PHP_MSHUTDOWN_FUNCTION(comp_inject)
+{
+    zend_compile_file = original_compile_file;
+    /*
+    if (inject_start_string != NULL) {
+        zend_string_release(inject_start_string);
+    }
+    if (inject_end_string) {
+        zend_string_release(inject_end_string);
+    }
+    */
 }
 
 /* {{{ comp_inject_module_entry
@@ -173,10 +294,10 @@ zend_module_entry comp_inject_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"comp_inject",					/* Extension name */
 	comp_inject_functions,			/* zend_function_entry */
-	NULL,							/* PHP_MINIT - Module initialization */
-	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
+	PHP_MINIT(comp_inject),							/* PHP_MINIT - Module initialization */
+	PHP_MSHUTDOWN(comp_inject),		/* PHP_MSHUTDOWN - Module shutdown */
 	PHP_RINIT(comp_inject),			/* PHP_RINIT - Request initialization */
-	NULL,							/* PHP_RSHUTDOWN - Request shutdown */
+	PHP_RSHUTDOWN(comp_inject),	    /* PHP_RSHUTDOWN - Request shutdown */
 	PHP_MINFO(comp_inject),			/* PHP_MINFO - Module info */
 	PHP_COMP_INJECT_VERSION,		/* Version */
     PHP_MODULE_GLOBALS(comp_inject),
--- tests/test.php
+++ tests/test.php
@@ -1,10 +1,3 @@
 <?php
-
-include 'test2.php';
-comp_inject_start(
-	"echo '/*# ' . __FILE__ . ':0 */' . PHP_EOL;", 
-	"echo '/*# ' . __FILE__ . ':$ */' . PHP_EOL;"
-);
-include 'test2.php';
-comp_inject_stop();
-include 'test2.php';
+comp_inject_start("%s\n", "");
+include("test5.php");
